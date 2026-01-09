dd40917957630a238fc02691039c972a3fa3f165
Kelly Ferrone <kelly@duplocloud.net>
2026-01-08 18:35:08 -0700

    fix(test): simplify autoConnect check in openvpn scenario

--- test/openvpn/install-only.sh
+++ test/openvpn/install-only.sh
@@ -8,6 +8,6 @@ source dev-container-features-test-lib
 check "openvpn is installed" command -v openvpn
 check "init script exists" test -f /usr/local/share/openvpn-init.sh
 check "post-start script exists" test -f /usr/local/share/openvpn-post-start.sh
-check "autoConnect persisted false" bash -c '[ "'"'"$(cat /usr/local/share/openvpn-autoconnect)"'"'" = "false" ]'
+check "autoConnect persisted false" grep -q "false" /usr/local/share/openvpn-autoconnect
 
 reportResults

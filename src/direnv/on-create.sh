2235dd865f7efe6677a394108143c76d0b258054
Kelly Ferrone <kelly@duplocloud.net>
2026-04-20 21:36:37 +0000

    fixed issue with loading the plugins correctl for direnv

--- src/direnv/on-create.sh
+++ src/direnv/on-create.sh
@@ -6,10 +6,11 @@ echo "Configuring direnv..."
 # Use devcontainer environment variables with fallbacks
 USER_HOME="${_REMOTE_USER_HOME:-$HOME}"
 
-# Add direnv bash hook to .bashrc
+# Add direnv config and bash hook to .bashrc
 cat <<EOF >> "${USER_HOME}/.bashrc"
 
 ## Sourced from Duplocloud Direnv Feature
+export DIRENV_CONFIG="${USER_HOME}/direnv"
 eval "\$(direnv hook bash)"
 
 EOF
